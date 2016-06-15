d4a91f8266e9e19c198b0b302f26868fa719e6e0
darren <764190362@qq.com>
2016-06-15 17:57:23 +0800

    finish05

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 
 import configureStore from './configureStore';
+import Root from './components/Root';
 
 const store = configureStore();
 
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
